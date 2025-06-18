f4d199c7b2eb14454e6a704bd9b1aeb12df0a9b6
greenmindagency <greenmindagency@gmail.com>
2025-06-18 19:02:29 +0300

    back

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 // This fxn allows plugins to insert themselves/scripts/css/files (right here) into the footer of your website. 
 // Removing this fxn call will disable all kinds of plugins. 
 // Move it if you like, but keep it around.
-?>
+?> 
 
 
       
