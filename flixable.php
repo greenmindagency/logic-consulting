1c34afbbf0edfae6b0ab86f3ba4361e967410987
Green Mind Agency <65516158+greenmindagency@users.noreply.github.com>
2025-06-19 13:01:50 +0300

    Update catslist slider read more style

--- flixable.php
+++ flixable.php
@@ -1476,8 +1476,9 @@ if (!isset($sliderCounter)) {
               <?php if ($description): ?>
                 <p class="card-text"><?php echo esc_html($description); ?></p>
               <?php endif; ?>
-              <a href="<?php echo esc_url($tag_link); ?>" class="btn btn-primary mt-3" aria-label="<?php echo esc_attr($cat->name); ?>">
-                <i class="text-white fa-solid fa-arrow-right-long"></i>
+              <a href="<?php echo esc_url($tag_link); ?>" class="mt-3 read-more-link" aria-label="<?php echo esc_attr($cat->name); ?>">
+                <span class="read-more-text me-1">Read more</span>
+                <img class="read-more-arrow" src="https://logic-consulting.com/logicconsulting/wp-content/uploads/2025/06/LOGIC-Consulting-Arrow.png" width="30" height="30" alt="Arrow"/>
               </a>
             </div>
           </div>
