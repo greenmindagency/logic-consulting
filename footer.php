d75a82bd4afb6526de55e8ce3d7af8ba8c68be0a
greenmindagency <greenmindagency@gmail.com>
2025-06-19 10:58:20 +0300

    fix jaralex opacity to videos

--- footer.php
+++ footer.php
@@ -269,7 +269,7 @@ a:active {text-decoration: none;}
 h1,h2,h3,h4,h5,h6 {font-weight:600 !important} 
      
 .card-overlay img, .jarallax-img {opacity: 0.3} 
-.jarallax iframe {opacity: 0.5 }
+.jarallax iframe {opacity: 0.8 }
 
 .video-jarallax-content {padding: 8rem 3rem}
 
